a3d8fb420a31f56d962cf3ddd96835dd521b605c
Youssef BADDI <baddiyoussef85@gmail.com>
2026-05-02 13:04:31 +0100

    commit all

---
+++ example_4/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <HelloWorld />
+</template>
+
+<!-- 
+one way binding:
+- interpolation : {{ message }}
+- v-bind : <img v-bind:src="imageSrc"> </img>
+- event binfing v-on : <button v-on:click="doSomething">Click me</button>
+
+two way binding : v-model 
+
+-->
