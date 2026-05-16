af57663c2e155b208be08cd11a45ff190bcd37eb
Youssef BADDI <baddiyoussef85@gmail.com>
2026-05-16 10:44:45 +0100

    add v-for v-if directives

--- example_1/script.js
+++ example_1/script.js
@@ -2,8 +2,14 @@ const App = Vue.createApp({
   data() {
     return {
       message: "<span>Hello world</span>",
-      tab: [2, 3, 5, 8],
+      tab: [2, 4, 5, 8],
       personne: { id: 100, nom: "baddi", prenom: "youssef" },
+      personnes: [
+        { id: 100, nom: "baddi1", prenom: "youssef1" },
+        { id: 101, nom: "baddi2", prenom: "youssef2" },
+        { id: 102, nom: "baddi3", prenom: "youssef3" },
+        { id: 103, nom: "baddi4", prenom: "youssef4" },
+      ],
       name: "baddi youssef",
     };
   },
